fd6504302bfc1fcec84add7bcb55ac9f4c43668d
maemual <maemual@gmail.com>
2013-08-25 23:10:31 +0800

    Modify some files.

--- MovieManger.Test/Properties/AssemblyInfo.cs
+++ MovieManger.Test/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyTitle("MovieManger.Test")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyProduct("MovieManger.Test")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 如果此项目向 COM 公开，则下列 GUID 用于类型库的 ID
-[assembly: Guid("2aa23579-4247-499a-8d2f-450e9fde3eeb")]
+[assembly: Guid("01c158c1-9f9d-4137-b720-167653f82386")]
 
 // 程序集的版本信息由下面四个值组成:
 //
